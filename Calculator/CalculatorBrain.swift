286fc265447fb8ee665428b5b2dd6205ab62114f
Dianwen Li <dianwen.li@duke.edu>
2015-04-23 15:05:23 -0400

    Initial commit

---
+++ Calculator/CalculatorBrain.swift
@@ -0,0 +1,201 @@
+//
+//  CalculatorBrain.swift
+//  Calculator
+//
+//  Created by Dan Li  on 4/10/15.
+//  Copyright (c) 2015 Dan Li . All rights reserved.
+//
+
+import Foundation
+
+class CalculatorBrain: Printable {
+    
+    private enum Op: Printable {
+        case Operand(Double)
+        case Constant(String, Double)
+        case Variable(String)
+        case UnaryOperation(String, Double -> Double)
+        case BinaryOperation(String, (Double, Double) -> Double)
+        
+        var description: String {
+            get {
+                switch self {
+                case Op.Operand(let operand):
+                    return "\(operand)"
+                case Op.Constant(let symbol,_):
+                    return "\(symbol)"
+                case Op.Variable(let symbol):
+                    return "\(symbol)"
+                case Op.UnaryOperation(let symbol, _):
+                    return "\(symbol)"
+                case Op.BinaryOperation(let symbol,_):
+                    return "\(symbol)"
+                }
+            }
+        }
+    }
+    
+    var description: String {
+        get {
+            return getDescriptionFromStack(opStack)
+        }
+    }
+    
+    private func getDescriptionFromStack(stack: [Op]) -> String {
+        var stackString = [String]()
+        for i in 0..<stack.count {
+            let op = stack[i]
+            switch op {
+            case Op.UnaryOperation(let symbol, _):
+                if (stack.count > 1) {
+                    var newExpression = "\(symbol)\(stackString[stackString.endIndex-1])"
+                    var parenthesisInsertionIndex = advance(newExpression.startIndex, countElements(symbol))
+                    if !stackString[stackString.endIndex-1].hasSuffix(")") {
+                        newExpression.insert("(", atIndex: parenthesisInsertionIndex)
+                        newExpression.insert(")", atIndex: newExpression.endIndex)
+                    }
+                    stackString.removeAtIndex(stackString.endIndex-1)
+                    if(stackString.count > 0) {
+                        stackString.append(", ")
+                    }
+                    stackString.append(newExpression)
+                }
+                else {
+                    stackString = ["\(symbol)(?)"]
+                }
+            case Op.BinaryOperation(let symbol, _):
+                if (stackString.count >= 2 && stackString[stackString.endIndex-2] != ", " && stackString[stackString.endIndex-1] != ", ") {
+                    let newExpression = "(\(stackString[stackString.endIndex-2])\(symbol)\(stackString[stackString.endIndex-1]))"
+                    stackString.removeAtIndex(stackString.endIndex-1)
+                    stackString.removeAtIndex(stackString.endIndex-1)
+                    if(stackString.count > 0) {
+                        stackString.append(", ")
+                    }
+                    stackString.append(newExpression)
+                }
+                else if (stackString.count == 1 && stackString[stackString.endIndex-1] != ", ") {
+                    let newExpression = "(?\(symbol)\(stackString[stackString.endIndex-1]))"
+                    stackString.removeAtIndex(stackString.endIndex-1)
+                    stackString.append(newExpression)
+                }
+                else {
+                    stackString.append("?\(symbol)?")
+                }
+            default:
+                stackString.append(op.description)
+            }
+        }
+        stackString.append(" =")
+        return String().join(stackString)
+    }
+    
+    private var opStack = [Op]()
+    
+    private var knownOps = [String:Op]()
+    
+    private var variableValues = [String:Double]()
+    
+    init() {
+        func learnOp(op: Op) {
+            knownOps[op.description] = op
+        }
+        learnOp(Op.BinaryOperation("×", *))
+        learnOp(Op.BinaryOperation("÷") { $1 / $0 })
+        learnOp(Op.BinaryOperation("−") { $1 - $0 })
+        learnOp(Op.BinaryOperation("+", +))
+        learnOp(Op.UnaryOperation("√", sqrt))
+        learnOp(Op.UnaryOperation("cos", cos))
+        learnOp(Op.UnaryOperation("sin", sin))
+        learnOp(Op.Constant("π", M_PI))
+        
+    }
+    
+    var program: AnyObject { // guarenteed to be a PropertyList
+        get {
+            return opStack.map({$0.description})
+        }
+        set {
+            if let opSymbols = newValue as? [String] {
+                var newOpStack = [Op]()
+                for opSymbol in opSymbols {
+                    if let op = knownOps[opSymbol] {
+                        newOpStack.append(op)
+                    }
+                    if let variableValue = variableValues[opSymbol] {
+                        newOpStack.append(Op.Variable(opSymbol))
+                    }
+                    else if let operand = NSNumberFormatter().numberFromString(opSymbol)?.doubleValue {
+                        newOpStack.append(Op.Operand(operand))
+                    }
+                }
+                opStack = newOpStack
+            }
+        }
+    }
+    
+    private func evaluate(ops: [Op]) -> (result: Double?, remainingOps: [Op]) {
+        if !ops.isEmpty {
+            var remainingOps = ops
+            let op = remainingOps.removeLast()
+            switch op {
+            case Op.Operand(let operand):
+                return (operand, remainingOps)
+            case Op.Constant(_, let value):
+                return (value, remainingOps)
+            case Op.Variable(let name):
+                return (variableValues[name], remainingOps)
+            case Op.UnaryOperation(_, let operation):
+                let operandEvaluation = evaluate(remainingOps)
+                if let operand = operandEvaluation.result {
+                    return (operation(operand), operandEvaluation.remainingOps)
+                }
+            case Op.BinaryOperation(_, let operation):
+                let op1Evaluation = evaluate(remainingOps)
+                if let operand1 = op1Evaluation.result {
+                    let op2Evaluation = evaluate(op1Evaluation.remainingOps)
+                    if let operand2 = op2Evaluation.result {
+                        return (operation(operand1, operand2), op2Evaluation.remainingOps)
+                    }
+                }
+            }
+        }
+        return (nil, ops)
+    }
+    
+    func evaluate() -> Double? {
+        let (result, remainder) = evaluate(opStack)
+        println("\(opStack) = \(result) with \(remainder) left over")
+        return result
+    }
+    
+    func pushOperand(operand: Double) -> Double? {
+        opStack.append(Op.Operand(operand))
+        return evaluate()
+    }
+    
+    func pushOperand(symbol: String) -> Double? {
+        opStack.append(Op.Variable(symbol))
+        return evaluate()
+    }
+    
+    func saveVariable(symbol: String, value: Double) -> Double? {
+        variableValues[symbol] = value
+        return evaluate()
+    }
+    
+    func performOperation(symbol: String) -> Double? {
+        if let operation = knownOps[symbol] {
+            opStack.append(operation)
+        }
+        return evaluate()
+    }
+    
+    func clearStack() {
+        opStack = [Op]()
+    }
+    
+    func clearVariables() {
+        variableValues = [String:Double]()
+    }
+    
+}
--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -9,17 +9,89 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    @IBOutlet weak var display: UILabel!
+    
+    @IBOutlet weak var stackDisplay: UILabel!
+    
+    var userIsInTheMiddleOfTypingANumber: Bool = false
+    
+    var brain = CalculatorBrain()
+    
+    @IBAction func appendDigit(sender: UIButton) {
+        let digit = sender.currentTitle!
+        if userIsInTheMiddleOfTypingANumber {
+            if let newDisplay = NSNumberFormatter().numberFromString(display.text! + digit){
+                display.text = display.text! + digit
+            }
+        }
+        else {
+            display.text = digit
+            userIsInTheMiddleOfTypingANumber = true
+        }
+    }
+    
+    @IBAction func operate(sender: UIButton) {
+        if userIsInTheMiddleOfTypingANumber {
+            enter()
+        }
+        if let operation = sender.currentTitle {
+            displayValue = brain.performOperation(operation)
+        }
+        updateStackDisplay()
+    }
+    
+    func updateStackDisplay() {
+        stackDisplay.text = brain.description
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func enter() {
+        userIsInTheMiddleOfTypingANumber = false
+        if let displayValueToEval = displayValue {
+            var result = brain.pushOperand(displayValueToEval)
+            displayValue = result
+        }
+        updateStackDisplay()
+     }
+    
+    @IBAction func clearDisplay() {
+        userIsInTheMiddleOfTypingANumber = false
+        display.text = "0"
+        brain.clearStack()
+        brain.clearVariables()
+        stackDisplay.text = " ="
     }
-
-
+    
+    @IBAction func saveM() {
+        userIsInTheMiddleOfTypingANumber = false
+        if let unwrappedDisplayValue = displayValue {
+            if let result = brain.saveVariable("M", value: unwrappedDisplayValue) {
+                displayValue = result
+            }
+        }
+        updateStackDisplay()
+    }
+    
+    @IBAction func retrieveM() {
+        if userIsInTheMiddleOfTypingANumber {
+            enter()
+        }
+        displayValue = brain.pushOperand("M")
+        updateStackDisplay()
+    }
+    
+    var displayValue: Double? {
+        get {
+            return NSNumberFormatter().numberFromString(display.text!)?.doubleValue
+        }
+        set {
+            if let newDisplayValue = newValue {
+                display.text = "\(newDisplayValue)"
+            }
+            else {
+                display.text = " "
+            }
+            userIsInTheMiddleOfTypingANumber = false
+        }
+    }
 }
 
